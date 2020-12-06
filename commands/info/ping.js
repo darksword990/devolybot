1e8c31bf9ce27dd4285471f9552aed39edd69b4c
darksword990 <71089813+darksword990@users.noreply.github.com>
2020-12-06 21:37:30 +0500

    hi

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'pong',
     run: async (client, message, args, mongoose) => {
-        message.client.send('pong')
+        message.channel.send('pong')
     }
 }
