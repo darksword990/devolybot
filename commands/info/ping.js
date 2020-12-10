0943de21f77336a9c875efa4bc24b685b3911ac7
darksword990 <71089813+darksword990@users.noreply.github.com>
2020-12-10 13:01:29 +0500

    hi

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (client, message, args) => {
         message.channel.send('Pong!').then(msg => {
             const ping = msg.createdTimestamp - message.createdTimestamp
-            msg.edit({embed: {author: {name: message.author.tag, icon_url: message.author.displayAvatarURL({format: `png`, dynamic: true})}, description: `:white_check_mark:\nPing: ${ping}ms\nAPI: ${client.ws.ping}`}})
+            msg.edit({embed: {color: 0x00ff00,author: {name: message.author.tag, icon_url: message.author.displayAvatarURL({format: `png`, dynamic: true})}, description: `:white_check_mark:\n**Ping:** ${ping}ms\n**API:** ${client.ws.ping}`}})
         })
     }
 }
