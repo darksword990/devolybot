680dfde3bbaaed078d72ab8b21ad375057a4a631
darksword990 <71089813+darksword990@users.noreply.github.com>
2020-12-11 21:44:50 +0500

    hi

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
-
-
 const path = require('path')
 const fs = require('fs')
 const Discord = require('discord.js')
